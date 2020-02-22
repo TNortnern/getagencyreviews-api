dda812b0e8c2e71b9f03f080ee1d85ad44dff343
TNORTHERN <traynorthern@yahoo.com>
2020-02-22 10:02:27 -0600

    url changed in email so that it can be updated everywhere in one line

--- resources/views/reviews/email.blade.php
+++ resources/views/reviews/email.blade.php
@@ -370,31 +370,31 @@
                                             <div
                                                 style="font-family:Roboto, sans-serif;font-size:16px;line-height:33px;text-align:center;color:black;">
                                                 <a style="color: #464646; text-decoration: none;"
-                                                    href="http://localhost:3000/reviews?rating=1-{{ $email->id }}-{{ $agent->id }}">
+                                                    href="{{ env('CLIENT_URL') }}/reviews?rating=1-{{ $email->id }}-{{ $agent->id }}">
                                                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Five-pointed_star.svg/2000px-Five-pointed_star.svg.png"
                                                         style="width:50px"
                                                         target="__blank">
                                                 </a>
                                                 <a style="color: #464646; text-decoration: none;"
-                                                    href="http://localhost:3000/reviews?rating=2-{{ $email->id }}-{{ $agent->id }}"
+                                                    href="{{ env('CLIENT_URL') }}/reviews?rating=2-{{ $email->id }}-{{ $agent->id }}"
                                                     target="__blank">
                                                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Five-pointed_star.svg/2000px-Five-pointed_star.svg.png"
                                                         style="width:50px">
                                                 </a>
                                                 <a style="color: #464646; text-decoration: none;"
-                                                    href="http://localhost:3000/reviews?rating=3-{{ $email->id }}-{{ $agent->id }}"
+                                                    href="{{ env('CLIENT_URL') }}/reviews?rating=3-{{ $email->id }}-{{ $agent->id }}"
                                                     target="__blank">
                                                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Five-pointed_star.svg/2000px-Five-pointed_star.svg.png"
                                                         style="width:50px">
                                                 </a>
                                                 <a style="color: #464646; text-decoration: none;"
-                                                    href="http://localhost:3000/reviews?rating=4-{{ $email->id }}-{{ $agent->id }}"
+                                                    href="{{ env('CLIENT_URL') }}/reviews?rating=4-{{ $email->id }}-{{ $agent->id }}"
                                                     target="__blank">
                                                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Five-pointed_star.svg/2000px-Five-pointed_star.svg.png"
                                                         style="width:50px">
                                                 </a>
                                                 <a style="color: #464646; text-decoration: none;"
-                                                    href="http://localhost:3000/reviews?rating=5-{{ $email->id }}-{{ $agent->id }}"
+                                                    href="{{ env('CLIENT_URL') }}/reviews?rating=5-{{ $email->id }}-{{ $agent->id }}"
                                                     target="__blank">
                                                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Five-pointed_star.svg/2000px-Five-pointed_star.svg.png"
                                                         style="width:50px">
