16856cd932e113dad33a79cf9710a17fbf44aa75
TNORTHERN <traynorthern@yahoo.com>
2020-02-29 10:55:38 -0600

    fully working review system

--- app/Http/Controllers/ReviewRequestController.php
+++ app/Http/Controllers/ReviewRequestController.php
@@ -60,28 +60,32 @@ class ReviewRequestController extends Controller
     {
         if ($request->has('link_clicked')) {
             ReviewRequest::where('id', $id)->update(['link_clicked' => date('Y-m-d H:i:s')]);
-            return response()->json('Updated link clicked' . $id, 200);
+            $review = ReviewRequest::where('id', $id)->with('agent')->first();
+            return $review;
         }
         if ($request->has('star_rating_completed')) {
             ReviewRequest::where('id', $id)->update([
                 'star_rating_completed' => date('Y-m-d H:i:s'),
                 'star_rating' => $request->star_rating
             ]);
-            return response()->json('Updated rating' . $id, 200);
+            $review = ReviewRequest::where('id', $id)->with('agent')->first();
+            return $review;
         }
         if ($request->has('feedback_completed')) {
             ReviewRequest::where('id', $id)->update([
                 'feedback_completed' => date('Y-m-d H:i:s'),
                 'feedback' => $request->feedback
             ]);
-            return response()->json('Updated feedback' . $id, 200);
+            $review = ReviewRequest::where('id', $id)->with('agent')->first();
+            return $review;
         }
         if ($request->has('external_review_completed')) {
             ReviewRequest::where('id', $id)->update([
                 'external_review_completed' => date('Y-m-d H:i:s'),
                 'external_link_clicked' => date('Y-m-d H:i:s')
             ]);
-            return response()->json('Updated external' . $id, 200);
+            $review = ReviewRequest::where('id', $id)->with('agent')->first();
+            return $review;
         }
     }
 }
